fc635ea96fe4cc9b88a214581b0929a2826fe80d
Shodipeadeolu <120527203+Shodipeadeolu@users.noreply.github.com>
2026-03-15 13:32:08 -0400

    Fintrack rebuild

--- src/components/FAB.jsx
+++
@@ -1,9 +0,0 @@
-import './FAB.css'
-
-export default function FAB({ onClick }) {
-  return (
-    <button className="fab" onClick={onClick} aria-label="Add transaction">
-      <span className="fab-icon">+</span>
-    </button>
-  )
-}
